175138d4872b50d5e0c45b5caa1979b4275d1a32
gyesswhat <h2weirdo@icloud.com>
2025-07-04 14:30:28 +0900

    day5: 에러 핸들링

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from datetime import datetime, timedelta, time
 from typing import Union, List, Literal, Annotated, Set, Dict, Any
 from uuid import UUID
 
-from pydantic import BaseModel, Field, HttpUrl, EmailStr
-from fastapi import FastAPI, Query, Path, Body, Cookie, Header, status, Form, File, UploadFile
+from pydantic import *
+from fastapi import *
+from fastapi.responses import JSONResponse
 from enum import Enum
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -729,4 +733,85 @@ class CommonHeaders(BaseModel):
 #         "token": token,
 #         "fileb_content_type": fileb.content_type,
 #     }
+
+items = {"foo": "The Foo Wrestlers"}
+
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str):
+#     if item_id not in items:
+#         raise HTTPException(status_code=404, detail="Item not found")
+#     return {"item": items[item_id]}
+#
+# @app.get("/items-header/{item_id}")
+# async def read_item_header(item_id: str):
+#     if item_id not in items:
+#         raise HTTPException(
+#             status_code=404,
+#             detail="Item not found",
+#             headers={"X-Error": "There goes my error"},
+#         )
+#     return {"item": items[item_id]}
+
+# class UnicornException(Exception):
+#     def __init__(self, name: str):
+#         self.name = name
+#
+# @app.exception_handler(UnicornException)
+# async def unicorn_exception_handler(request: Request, exc: UnicornException):
+#     return JSONResponse(
+#         status_code=418,
+#         content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+#     )
+#
+#
+# @app.get("/unicorns/{name}")
+# async def read_unicorn(name: str):
+#     if name == "yolo":
+#         raise UnicornException(name=name)
+#     return {"unicorn_name": name}
+
+# @app.exception_handler(StarletteHTTPException)
+# async def http_exception_handler(request, exc):
+#     return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+#
+#
+# @app.exception_handler(RequestValidationError)
+# async def validation_exception_handler(request, exc):
+#     return JSONResponse(
+#         status_code=400,
+#         content={
+#             "message": "Invalid input",
+#             "details": exc.errors()
+#         }
+#     )
+#
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     if item_id == 3:
+#         raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+#     return {"item_id": item_id}
+
+# from fastapi import FastAPI, Request, status
+# from fastapi.encoders import jsonable_encoder
+# from fastapi.exceptions import RequestValidationError
+# from fastapi.responses import JSONResponse
+# from pydantic import BaseModel
+#
+# @app.exception_handler(RequestValidationError)
+# async def validation_exception_handler(request: Request, exc: RequestValidationError):
+#     return JSONResponse(
+#         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+#         content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+#     )
+#
+#
+# class Item(BaseModel):
+#     title: str
+#     size: int
+#
+#
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     return item
 
